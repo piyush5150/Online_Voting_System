188fb5725d2a40ce9b6815fbcb20a2bde00b46ac
Piyush patidar <piyushpatidar5150@gmail.com>
2025-05-10 23:47:54 +0530

    2nd commit

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%6lp_p!%r$7t-2ql5hc5(r@)8u_fc+6@ugxcnz=h=b(fn#3$p9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onlinevoting.onrender.com']
 
 
 # Application definition
